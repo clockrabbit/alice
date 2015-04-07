9f8b7f352f46202cbff2a02c6b4219e3a7f2693a
clockrabbit <wingmoon@outlook.com>
2015-04-07 10:56:37 +0800

    BMI
    測試不嫌少

---
+++ BMI.R
@@ -0,0 +1,21 @@
+height_vec = c(180,169,173)
+height_vec=height_vec/100
+height_vec
+
+
+height_vec = height_vec^2
+height_vec
+
+
+weight_vec = c(73,87,43)
+weight_vec
+
+BMI = weight_vec/height_vec
+BMI
+names(BMI)=c("Brain","Tody","Sherry")
+BMI
+
+24>BMI &BMI>=18.5
+
+
+sum(seq(1,1000))
